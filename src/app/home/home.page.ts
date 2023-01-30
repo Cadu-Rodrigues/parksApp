9e5b51aad409db965007a914385c05738983b7f2
Carlos Eduardo <carloseduardordesousa@gmail.com>
2023-01-30 02:13:42 -0300

    chore: initial commit

---
+++ src/app/home/home.page.ts
@@ -0,0 +1,15 @@
+import { Component } from '@angular/core';
+import { APIResponse } from '../models/APIResponse';
+import { Park } from '../models/Park';
+
+@Component({
+  selector: 'app-home',
+  templateUrl: 'home.page.html',
+  styleUrls: ['home.page.scss'],
+})
+export class HomePage {
+  constructor() {
+  }
+
+
+}
