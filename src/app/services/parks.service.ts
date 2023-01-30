60a96deba59f9adc7fb51f5ad69b6c6274e55a45
Carlos Eduardo <carloseduardordesousa@gmail.com>
2023-01-30 02:43:09 -0300

    refactor: Adiciona versão do aplicativo nas requisições

--- src/app/services/parks.service.ts
+++ src/app/services/parks.service.ts
@@ -1,10 +1,9 @@
-import { HttpClient, HttpResponse } from '@angular/common/http';
+import { HttpClient, HttpHeaders, HttpResponse } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { Observable } from 'rxjs';
 import { environment } from 'src/environments/environment';
 import { APIResponse } from '../models/APIResponse';
 import { Geolocation } from '@capacitor/geolocation';
-import { Park } from '../models/Park';
 import { ParkResponse } from '../models/ParkResponse';
 
 @Injectable({
@@ -13,27 +12,36 @@ import { ParkResponse } from '../models/ParkResponse';
 export class ParksService {
   constructor(private http: HttpClient) {}
   getParks(): Observable<APIResponse> {
-    return this.http.get<APIResponse>(environment.APILink);
+    let headers = new HttpHeaders();
+    headers = headers.set('app-version', '1.0');
+    const options = { headers };
+    return this.http.get<APIResponse>(environment.APILink, options);
   }
-  async calculateDistance(park:ParkResponse): Promise<number> {
+  async calculateDistance(park: ParkResponse): Promise<number> {
     const coordinates = await Geolocation.getCurrentPosition();
-    return this.getDistanceInKm(coordinates.coords.latitude,coordinates.coords.longitude,parseFloat(park.lat),parseFloat(park.lng));
+    return this.getDistanceInKm(
+      coordinates.coords.latitude,
+      coordinates.coords.longitude,
+      parseFloat(park.lat),
+      parseFloat(park.lng)
+    );
   }
   getDistanceInKm(lat1: number, lon1: number, lat2: number, lon2: number) {
     var R = 6371; // Radius of the earth in km
-    var dLat = this.deg2rad(lat2-lat1);  // deg2rad below
-    var dLon = this.deg2rad(lon2-lon1); 
-    var a = 
-      Math.sin(dLat/2) * Math.sin(dLat/2) +
-      Math.cos(this.deg2rad(lat1)) * Math.cos(this.deg2rad(lat2)) * 
-      Math.sin(dLon/2) * Math.sin(dLon/2)
-      ; 
-    var c = 2 * Math.atan2(Math.sqrt(a), Math.sqrt(1-a)); 
+    var dLat = this.deg2rad(lat2 - lat1); // deg2rad below
+    var dLon = this.deg2rad(lon2 - lon1);
+    var a =
+      Math.sin(dLat / 2) * Math.sin(dLat / 2) +
+      Math.cos(this.deg2rad(lat1)) *
+        Math.cos(this.deg2rad(lat2)) *
+        Math.sin(dLon / 2) *
+        Math.sin(dLon / 2);
+    var c = 2 * Math.atan2(Math.sqrt(a), Math.sqrt(1 - a));
     var d = R * c; // Distance in km
     return d;
   }
-  
+
   deg2rad(deg: number) {
-    return deg * (Math.PI/180)
+    return deg * (Math.PI / 180);
   }
 }
